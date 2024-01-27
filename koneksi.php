9406ae5a845c7998f94d9314415d568eff1921c2
noraananda <anandanora9@gmail.com>
2024-01-26 20:04:19 -0800

    first commit

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$conn = mysqli_connect("localhost","root","","kampus");
+   // if($conn == true){
+    //    echo"Berhasil terkoneksi ke database";
+   // }else{
+   //     echo"Gagal terkoneksi ke database";
+   // }
+
+
+?>
